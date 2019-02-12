4f4a5167281a1c43f3cfcaa3b2fff948348e81a2
hendroprwk08 <hendroprwk08@gmail.com>
2019-02-12 21:22:18 +0700

    ok

--- app/src/main/java/com/example/cilodong_latihan02_recyclerview/MainActivity.java
+++ app/src/main/java/com/example/cilodong_latihan02_recyclerview/MainActivity.java
@@ -1,26 +1,22 @@
 package com.example.cilodong_latihan02_recyclerview;
 
-import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.DialogInterface;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -99,6 +95,14 @@ public class MainActivity extends AppCompatActivity {
                                 .setContentText(edtNama.getText().toString() +" ditambah");
 
                         notificationManager.notify(notificationId, mBuilder.build());
+
+                        //sementara aja
+                        for (int i = 0; i < siswaArrayList.size() ; i++) {
+                            Log.d("Tes nama ", siswaArrayList.get(i).getNama());
+                            Log.d("Tes IK", siswaArrayList.get(i).getIK().toString());
+                            Log.d("Tes SI", siswaArrayList.get(i).getSI().toString());
+                            Log.d("Tes TI", siswaArrayList.get(i).getTI().toString());
+                        }
 
                         //Toast.makeText(getBaseContext(),"Data tersimpan", Toast.LENGTH_SHORT).show();
                     }
